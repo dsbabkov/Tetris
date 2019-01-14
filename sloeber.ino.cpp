68dfaf25595b9b6ea8729687beef5f10d4457b36
Dmitriy Babkov <dsbabkov@gmail.com>
2019-01-14 23:04:00 +0300

    Проект завёрнут в sloeber проект

---
+++ sloeber.ino.cpp
@@ -0,0 +1,17 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2019-01-14 23:02:08
+
+#include "Arduino.h"
+#include "NokiaLCD.h"
+
+byte reverse(byte b) ;
+void setup() ;
+void loop() ;
+
+
+#include "nokia_5100_r03.ino"
+
+#endif
